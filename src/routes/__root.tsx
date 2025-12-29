95a15683f4585d29b95caa64a4f53859b8d8c015
Jumpei Ogawa <git@phanective.org>
2025-12-29 10:32:15 +0900

    feat: make main content area wider

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -92,7 +92,7 @@ function Root() {
           <div className="flex mx-auto lg:flex-row lg:w-fit lg:gap-2">
             <Navbar />
             <div className="flex justify-center mx-auto">
-              <div className="flex flex-col sm:border-x w-screen md:w-137.5">
+              <div className="flex flex-col sm:border-x w-screen md:max-w-5xl">
                 <ErrorBoundary>
                   <Outlet key="app" />
                 </ErrorBoundary>
