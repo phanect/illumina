09f68e78f385f9173e309ba4f2b0736ee4ca180f
Jumpei Ogawa <git@phanective.org>
2025-12-30 21:36:57 +0900

    refactor: replace deprecated `TanStackRouterVite` with `tanstackRouter`

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import ogPlugin from 'vite-plugin-open-graph';
 import { name, description } from './package.json';
@@ -9,7 +9,7 @@ import { name, description } from './package.json';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({
+    tanstackRouter({
       routeFileIgnorePattern: 'components',
     }),
     react({}),
