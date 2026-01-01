34038aad35dcddd9ccd25af2b172eee48e82bf05
Jumpei Ogawa <git@phanective.org>
2026-01-01 12:21:03 +0900

    fix: type error in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ import animate from "tailwindcss-animate";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: ['selector'],
+  darkMode: "selector",
   theme: {
     extend: {
       borderRadius: {
