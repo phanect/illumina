d3526438bb4e73a3d97667c9fd367f6cec522d93
Jumpei Ogawa <git@phanective.org>
2026-01-02 06:17:11 +0900

    chore: upgrade  to `@tanstack/react-router-devtools`
    This package was renamed.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { createRootRoute, Outlet, redirect } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import '../index.css';
 import { ErrorBoundary } from '../components/error-boundary';
 import { useBlueskyStore } from '../lib/bluesky/store';
