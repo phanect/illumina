f7a6b7b86c8dc42572982d15a846107c29008bb2
Jumpei Ogawa <git@phanective.org>
2026-01-02 10:11:50 +0900

    chore: set up ESLint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,30 +1,23 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import { defineConfig } from "eslint/config";
+import { core } from "@phanect/lint";
+import { react } from "@phanect/lint-react";
+import { defineConfig, type Config } from "eslint/config";
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
 import i18next from 'eslint-plugin-i18next';
-import reactPlugin from 'eslint-plugin-react';
 import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 
-const configs = defineConfig(tseslint.config(
-  i18next.configs['flat/recommended'],
-  reactPlugin.configs.flat.recommended,
-  { ignores: ['dist', '**/*.test.{ts,tsx}'] },
+const configs = defineConfig([
+  i18next.configs['flat/recommended'] as Config,
+  { ignores: ['dist'] },
+
+  ...core,
+  ...react,
+
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
     plugins: {
-      'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       // React 17 does not require importing React for JSX
       'react/react-in-jsx-scope': 'off',
@@ -66,6 +59,15 @@ const configs = defineConfig(tseslint.config(
     },
   },
   {
+    // Do not add `files: [ "*" ],` here.
+
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+
     settings: {
       react: {
         version: 'detect',
@@ -89,6 +91,6 @@ const configs = defineConfig(tseslint.config(
       ],
     },
   },
-));
+]);
 
 export default configs;
