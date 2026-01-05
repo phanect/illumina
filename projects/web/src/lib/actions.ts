6a8e9ed3ca633bb9540aaffc040f826f7121d3b1
Jumpei Ogawa <git@phanective.org>
2026-01-06 08:25:23 +0900

    doc: add JSDocs

--- projects/web/src/lib/actions.ts
+++ projects/web/src/lib/actions.ts
@@ -4,6 +4,11 @@ import createBlueskyClient from "@/lib/atproto";
 import getSession from "@/lib/iron";
 import { prisma } from "@/lib/prisma";
 
+/**
+ * Prepare to login with Bluesky and return Bluesky login URL
+ * @param handle - Bluesky handle
+ * @returns Login URL of Bluesky
+ */
 export async function loginWithBluesky(handle: string): Promise<string> {
   const blueskyClient = await createBlueskyClient(prisma);
   const blueskyLoginURL: URL = await blueskyClient.authorize(handle);
