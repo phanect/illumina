b0525735b5b58b6540f951daf040992d80318e2b
Jumpei Ogawa <git@phanective.org>
2026-01-05 17:47:51 +0900

    refactor: remove `cachedPrisma` for local environment

--- projects/web/src/lib/prisma.ts
+++ projects/web/src/lib/prisma.ts
@@ -1,17 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var cachedPrisma: PrismaClient;
-}
-
-let prismaClient: PrismaClient;
-if (process.env.NODE_ENV === "production") {
-  prismaClient = new PrismaClient();
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
-  }
-  prismaClient = global.cachedPrisma;
-}
-
-export const prisma = prismaClient;
+export const prisma = new PrismaClient();
