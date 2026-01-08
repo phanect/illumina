1787e77cf163d5ec1cbb676a5425118b2f2e0389
Jumpei Ogawa <git@phanective.org>
2026-01-09 07:56:30 +0900

    style: add missing .ts extensions for `import`s

--- projects/web-vite/src/i18n/index.ts
+++ projects/web-vite/src/i18n/index.ts
@@ -2,17 +2,17 @@ import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Pseudo from "i18next-pseudo";
 import { initReactI18next } from "react-i18next";
-import { de } from "./lang/de";
-import { en } from "./lang/en";
-import { es } from "./lang/es";
-import { fil } from "./lang/fil";
-import { fr } from "./lang/fr";
-import { ja } from "./lang/ja";
-import { nl } from "./lang/nl";
-import { ur } from "./lang/ur";
-import { yue } from "./lang/yue";
-import { zh_Hans } from "./lang/zh-hans";
-import { zh_Hant } from "./lang/zh-hant";
+import { de } from "./lang/de.ts";
+import { en } from "./lang/en.ts";
+import { es } from "./lang/es.ts";
+import { fil } from "./lang/fil.ts";
+import { fr } from "./lang/fr.ts";
+import { ja } from "./lang/ja.ts";
+import { nl } from "./lang/nl.ts";
+import { ur } from "./lang/ur.ts";
+import { yue } from "./lang/yue.ts";
+import { zh_Hans } from "./lang/zh-hans.ts";
+import { zh_Hant } from "./lang/zh-hant.ts";
 
 export const defaultNS = "app";
 
